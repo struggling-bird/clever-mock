ead7f13266f78ccb90dbf96e43733d94f42c2c24
董永强 <yqdong@37degree.com>
2018-08-22 11:01:36 +0800

    update

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // target: 'http://192.168.1.102:8080',// http://www.hxs-edu.com/
+  // target: '',// http://www.hxs-edu.com/
   target: 'http://www.hxs-edu.com',
   port: 8181,  // 服务端口
   autoMockCodes: [404], // 当代理失败时自动使用mock的，proxy server code

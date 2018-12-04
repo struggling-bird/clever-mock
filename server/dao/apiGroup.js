690b0e88d9d0613572f65d9ae56d4b9f6e85bdd8
董永强 <yqdong@37degree.com>
2018-12-04 17:21:23 +0800

    update

--- server/dao/apiGroup.js
+++ server/dao/apiGroup.js
@@ -7,7 +7,7 @@ module.exports = {
     let sql = 'select g.* from apigroup as g where g.project_id = ?'
     let groupList = await db.query(sql, [projectId])
     
-    sql = 'select a.id,a.name,a.path,a.method,a.group_id from api as a where a.project_id = ?'
+    sql = 'select a.id,a.name,a.path,a.method,a.group_id,a.description,a.params,a.res_structure from api as a where a.project_id = ?'
     let apiList = await db.query(sql, [projectId])
     let groupMap = {}
     groupList.forEach(group => {
--- web/src/modules/console/docView.vue
+++ web/src/modules/console/docView.vue
@@ -1,20 +1,54 @@
 <template>
-  <div>
-    文档xxx
+  <div class="doc-view">
+    <template v-for="group in groupList">
+      <div class="api-doc" v-for="api in group.apiList" :key="api.id">
+        <h3 v-if="api.name">{{api.name}}</h3>
+        <div class="api-url">
+          <span class="api-method" :class="api.method.toLowerCase()">{{api.method}}</span>{{api.path}}
+        </div>
+        <c-tabs v-if="api.description">
+          <c-tab-panel title="描述">
+            {{api.description}}
+          </c-tab-panel>
+        </c-tabs>
+        <c-tabs>
+          <c-tab-panel title="参数">
+            {{api.params}}
+          </c-tab-panel>
+        </c-tabs>
+        <c-tabs>
+          <c-tab-panel title="返回结构">
+            {{api.resStructure}}
+          </c-tab-panel>
+        </c-tabs>
+      </div>
+    </template>
   </div>
 </template>
 
 <script>
+import {actions} from '../../store/constants'
+import {mapState} from 'vuex'
 export default {
   name: 'docView',
   data () {
     return {
       msg: 'doc'
     }
+  },
+  computed: {
+    ...mapState({
+      groupList (state) {
+        return state.api.groupList
+      }
+    })
+  },
+  beforeCreate () {
+    this.$store.dispatch(actions.api.queryGroup, this.$route.params.id)
   }
 }
 </script>
 
 <style lang="sass">
-
+@import "styles/docView"
 </style>

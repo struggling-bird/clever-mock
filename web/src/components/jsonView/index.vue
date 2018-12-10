2e562907d0bb2f1ac0a9d92ff2c4d47293014655
董永强 <yqdong@37degree.com>
2018-12-10 17:24:01 +0800

    update

--- web/src/components/jsonView/index.vue
+++ web/src/components/jsonView/index.vue
@@ -1,40 +1,26 @@
 <template>
   <div class="json-view">
-    <template v-if="type === 'Object'">
-      <div class="json-line">
-        <span class="json-key" v-if="json.name">{{json.name}}: </span><span class="json-kh">{</span>
-      </div>
-      <div class="json-children">
-        <json-view v-for="field in json.children" :store="field"></json-view>
-      </div>
-      <div class="json-line">
-        <span class="json-kh">}</span>
-      </div>
-    </template>
-    <template v-else-if="type === 'Array'">
-      <div class="json-line">
-        <span class="json-key" v-if="json.name">{{json.name}}: </span><span class="json-kh">[</span>
-      </div>
-      <div class="json-line">
-        <span class="json-kh">]</span>
-      </div>
-    </template>
-    <template v-else>
-      <div class="json-line">
-        <span class="json-key">{{json.name}}: </span>
-        <span class="json-val">
+    <div class="json-line">
+      <span class="json-key" v-if="json.name">{{json.name}}: </span>
+      <span class="json-kh" v-if="['Object', 'Array'].includes(type)">{{type === 'Object' ? '{' : '['}}</span>
+      <span class="json-val">
           <template v-if="type === 'String'">"</template>
           {{json.value}}
           <template v-if="type === 'String'">"</template>
         </span>
-        <span class="json-annotate">
+      <span class="json-annotate" v-if="!['Object', 'Array'].includes(type)">
           <span class="json-xg">//</span>
-          <span class="json-annotation"><span class="json-annotation_">@required</span> {{json.required}}</span>
-          <span class="json-annotation"><span class="json-annotation_">@type</span> {{json.type}}</span>
-          <span class="json-annotation"><span class="json-annotation_">@desc</span>{{json.desc}}</span>
+          <span class="json-annotation" v-if="showRequired"><span class="json-annotation_">@required</span> {{json.required}}</span>
+          <span class="json-annotation" v-if="json.type"><span class="json-annotation_">@type</span> {{json.type}}</span>
+          <span class="json-annotation" v-if="json.desc"><span class="json-annotation_">@desc</span>{{json.desc}}</span>
         </span>
-      </div>
-    </template>
+    </div>
+    <div class="json-children" v-if="json.children && json.children.length">
+      <json-view v-for="field in json.children" :store="field"></json-view>
+    </div>
+    <div class="json-line" v-if="['Object', 'Array'].includes(type)">
+      <span class="json-kh">{{type === 'Object' ? '}' : ']'}}</span>
+    </div>
   </div>
 </template>
 
@@ -43,7 +29,11 @@ import {util} from '../../util'
 export default {
   name: 'jsonView',
   props: {
-    store: null
+    store: null,
+    showRequired: {
+      type: Boolean,
+      default: false
+    }
   },
   data () {
     return {
